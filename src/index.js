7a63ff4e3e82a05190bd783afa2a752124317b77
Avery Berry <AveryRBerry@gmail.com>
2023-08-29 19:25:53 -0400

    working on fixing time zones/ broken branch

--- src/index.js
+++ src/index.js
@@ -7,21 +7,57 @@ document.addEventListener("DOMContentLoaded", async function() {
     const demoWindSpeed = 30;
     const demoHours = 1; //temp for testing
     const demoUpdater = new UpdateCanvas(demoHours, demoWindSpeed);
-
+    
+    const searchForm = document.getElementById('searchForm');
     const container = document.getElementById('container');
-    const button = document.getElementById('startButton');
-    button.addEventListener('click', async function() {
-        container.style.display = 'none';
+    const button = document.getElementById('demoButton');
 
-        // const weatherData = await getWeather();
-        // const unixTimeStamp = weatherData.dt;
-        // const date = new Date(unixTimeStamp * 1000);
-        // const hours = date.getHours();
+    // button.addEventListener('click', async function(event) {
+    //     event.preventDefault();
+    //     container.style.display = 'none';
+
+    //     const weatherData = await getWeather("New York");
+    //     const utcShift = Math.ceil(weatherData.timezone/60/60)
+
+    //     const unixTimeStamp = weatherData.dt;
+    //     const date = new Date(unixTimeStamp * 1000);
+    //     let hours = date.getUTCHours() + utcShift;
+    //     if (hours>24) hours -= 24;
+    //     // const temp = weatherData.main.temp;
+    //     const windSpeed = weatherData.wind.speed;
+    //     // const weatherType = weatherData.weather[0].main;
+    //     const newUpdater = new UpdateCanvas(hours, windSpeed);
+    // })
+
+
+    searchForm.addEventListener('click', async function(event) {
+        event.preventDefault();
+        const searchInput = document.getElementById('searchInput');
+        const cityName = searchInput.value;
+
+        const weatherData = await getWeather(cityName);
+        const utcShift = Math.ceil(weatherData.timezone/60/60)
+        const unixTimeStamp = weatherData.dt;
+        const date = new Date(unixTimeStamp * 1000);
+        let hours = date.getUTCHours();
+        // if (hours>24) hours -= 24;
         // const temp = weatherData.main.temp;
-        // const windSpeed = weatherData.wind.speed;
+        const windSpeed = weatherData.wind.speed;
         // const weatherType = weatherData.weather[0].main;
-        // const newUpdater = new UpdateCanvas(hours, windSpeed);
-    })
+        const newUpdater = new UpdateCanvas(hours, windSpeed);
 
+        // console.log(date.getUTCHours())
+        // console.log("date.getUTCHours()")
+        // console.log(utcShift)
+        // console.log("utcShift")
+        console.log(hours)
+        console.log("hours")
+        // console.log(weatherData.timezone)
+        // console.log("weatherData.timezone")
+        // i need to mak an "if" date.getUTCHours() + utcShift > 
+
+        container.style.display = 'none';
+        searchInput.value = '';
+    })
 
 });
--- src/scripts/getWeatherData.js
+++ src/scripts/getWeatherData.js
@@ -1,9 +1,10 @@
 const API_KEY = "7dff519f917b5485d7f4dfe9e415db09";
-const API_URL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=New%20York"
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q="
 
-const getWeather = async () => {
-    const response = await fetch(`${API_URL}&appid=${API_KEY}`);
+const getWeather = async (cityName) => {
+    const response = await fetch(`${API_URL}${cityName}&appid=${API_KEY}`);
     const weatherData = await response.json();
+    console.log(weatherData)
     return weatherData;
 }
 export default getWeather
